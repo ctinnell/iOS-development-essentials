55de92663bf58422c335172884dc08b6362af0ca
Clay Tinnell <ctinnell@gaig.com>
2015-11-16 12:21:19 -0500

    Initial calls working

---
+++ 01-Swift-Asynchronous Network Access/01-Swift-Asynchronous Network Access/Model/TestCall.swift
@@ -0,0 +1,13 @@
+//
+//  TestCall.swift
+//  01-Swift-Asynchronous Network Access
+//
+//  Created by Clay Tinnell on 11/16/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class TestCall: NSObject {
+
+}
