e0295ede3afa8e719f62d6cb01e51bb526d6879e
Clay Tinnell <ctinnell@gaig.com>
2015-11-18 21:16:55 -0500

    Added Person Managed Object Subclass

---
+++ 02-Swift-Multithreaded Core Data/02-Swift-Multithreaded Core Data/Person+CoreDataProperties.m
@@ -0,0 +1,19 @@
+//
+//  Person+CoreDataProperties.m
+//  02-Swift-Multithreaded Core Data
+//
+//  Created by Clay Tinnell on 11/18/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Person+CoreDataProperties.h"
+
+@implementation Person (CoreDataProperties)
+
+@dynamic name;
+@dynamic phone;
+
+@end
---
+++ 02-Swift-Multithreaded Core Data/02-Swift-Multithreaded Core Data/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  02-Swift-Multithreaded Core Data
+//
+//  Created by Clay Tinnell on 11/18/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
