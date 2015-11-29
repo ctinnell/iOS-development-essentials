48f631115da5457a16c03d0623bd7b08c46573f1
Clay Tinnell <ctinnell@gaig.com>
2015-11-29 12:50:49 -0500

    Initial Project Configuration

---
+++ OAuth2Demo-04/OAuth2Demo-04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuth2Demo-04
+//
+//  Created by Clay Tinnell on 11/29/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
