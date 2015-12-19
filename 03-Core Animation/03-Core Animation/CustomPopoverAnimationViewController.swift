4628efab6a90d2806452b1140e6d5cd3ae4271c2
Clay Tinnell <ctinnell@gaig.com>
2015-12-19 10:04:46 -0500

    Finishing touches on animations

--- 03-Core Animation/03-Core Animation/CustomPopoverAnimationViewController.swift
+++ 03-Core Animation/03-Core Animation/CustomPopoverAnimationViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class CustomPopoverAnimationViewController: UIViewController, CustomPopoverViewControllerDelegate {
+class CustomPopoverAnimationViewController: UIViewController, CustomPopoverViewControllerDelegate, UIViewControllerTransitioningDelegate {
 
-    var popoverViewController: CustomPopoverViewController?
-    
+    private var popoverViewController: CustomPopoverViewController?
+    private let presentationAnimationController = CustomPopoverPresentationAnimationTransition()
+    private let dismissalAnimationController = CustomPopoverDismissalAnimationTransition()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,27 +32,78 @@ class CustomPopoverAnimationViewController: UIViewController, CustomPopoverViewC
         if let popoverViewController = popoverViewController {
             popoverViewController.delegate = self
             popoverViewController.modalPresentationStyle = .OverCurrentContext
+            popoverViewController.transitioningDelegate = self
             presentViewController(popoverViewController, animated: true, completion: nil)
         }
     }
     
     // MARK: - CustomPopoverViewControllerDelegate
     func okButtonPressed() {
+        dismissalAnimationController.animationMultiplier = -1.0
         self.popoverViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 
     func cancelButtonPressed() {
+        dismissalAnimationController.animationMultiplier = 1.0
         self.popoverViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return presentationAnimationController
     }
-    */
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return dismissalAnimationController
+    }
+    
+    private class CustomPopoverPresentationAnimationTransition: NSObject, UIViewControllerAnimatedTransitioning {
+        @objc private func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+            return 2.0
+        }
+        
+        @objc private func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+            let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+            
+            let finalFrame = transitionContext.finalFrameForViewController(toViewController)
+            let containerView = transitionContext.containerView()!
+            let bounds = UIScreen.mainScreen().bounds
+            toViewController.view.frame = CGRectOffset(finalFrame, 0, bounds.size.height)
+            containerView.addSubview(toViewController.view)
+            
+            UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .CurveLinear, animations: {
+                    toViewController.view.frame = finalFrame
+                }, completion: {
+                    finished in
+                    transitionContext.completeTransition(true)
+            })
+        }
+    }
+    
+    private class CustomPopoverDismissalAnimationTransition: NSObject, UIViewControllerAnimatedTransitioning {
+        var animationMultiplier = -1.0
 
+        @objc private func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+            return 3.0
+        }
+        
+        @objc private func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+            let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+            
+            var finalFrame = transitionContext.finalFrameForViewController(fromViewController)
+            let bounds = UIScreen.mainScreen().bounds
+            finalFrame = CGRectMake(finalFrame.origin.x, bounds.size.height, finalFrame.size.width, finalFrame.size.height)
+            let containerView = transitionContext.containerView()!
+            containerView.addSubview(fromViewController.view)
+            let radians = CGFloat((90.0 * animationMultiplier) * M_PI/180)
+            let transform = CGAffineTransformRotate(CGAffineTransformIdentity, radians)
+            
+            UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .CurveLinear, animations: {
+                    fromViewController.view.frame = finalFrame
+                    fromViewController.view.transform = transform
+                }, completion: {
+                    finished in
+                    transitionContext.completeTransition(true)
+            })
+        }
+    }
 }
--- 03-Core Animation/03-Core Animation/CustomPopoverViewController.swift
+++ 03-Core Animation/03-Core Animation/CustomPopoverViewController.swift
@@ -13,10 +13,9 @@ protocol CustomPopoverViewControllerDelegate {
     func cancelButtonPressed()
 }
 
-class CustomPopoverViewController: UIViewController, UIViewControllerTransitioningDelegate {
+class CustomPopoverViewController: UIViewController {
 
     var delegate: CustomPopoverViewControllerDelegate?
-    private let presentationAnimationController = CustomPopoverAnimationTransition()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +28,6 @@ class CustomPopoverViewController: UIViewController, UIViewControllerTransitioni
         // Dispose of any resources that can be recreated.
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-            let toViewController = segue.destinationViewController as UIViewController
-            toViewController.transitioningDelegate = self
-    }
-    
     @IBAction func okButtonPressed(sender: AnyObject) {
         delegate?.okButtonPressed()
     }
@@ -42,33 +36,6 @@ class CustomPopoverViewController: UIViewController, UIViewControllerTransitioni
         delegate?.cancelButtonPressed()
     }
     
-    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return presentationAnimationController
-    }
 
-    private class CustomPopoverAnimationTransition: NSObject, UIViewControllerAnimatedTransitioning {
-        @objc private func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-            return 2.0
-        }
-        
-        @objc private func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-            let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
-            let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
-            let finalFrameForVC = transitionContext.finalFrameForViewController(toViewController)
-            let containerView = transitionContext.containerView()!
-            let bounds = UIScreen.mainScreen().bounds
-            toViewController.view.frame = CGRectOffset(finalFrameForVC, 0, bounds.size.height)
-            containerView.addSubview(toViewController.view)
-            
-            UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .CurveLinear, animations: {
-                fromViewController.view.alpha = 0.5
-                toViewController.view.frame = finalFrameForVC
-                }, completion: {
-                    finished in
-                    transitionContext.completeTransition(true)
-                    fromViewController.view.alpha = 1.0
-            })
-        }
-
-    }
+    
 }
