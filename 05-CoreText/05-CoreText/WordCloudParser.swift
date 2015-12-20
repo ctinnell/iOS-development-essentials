d05aa6fc9a352cb41cbe017f436cac421fe4f178
Clay Tinnell <ctinnell@gaig.com>
2015-12-20 14:34:51 -0500

    Initial concept for WordCloudParser

---
+++ 05-CoreText/05-CoreText/WordCloudParser.swift
@@ -0,0 +1,52 @@
+//
+//  WordCloudParser.swift
+//  05-CoreText
+//
+//  Created by Clay Tinnell on 12/20/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class WordCloudParser: NSObject {
+    struct WordCloudElement {
+        var word: String
+        var count: Int
+    }
+    
+    static func wordCloudElements(text: String) -> [WordCloudElement] {
+        let words = text.stripSpecialCharacters().trimExtraWhiteSpace().componentsSeparatedByWhiteSpace().sort()
+        var wordCloudElements = [WordCloudElement]()
+        var previousWord = ""
+        
+        var wordCloudElement: WordCloudElement?
+        
+        for word in words {
+            if word == previousWord {
+                wordCloudElement?.count++
+            }
+            else {
+                if let safeWordCloudElement = wordCloudElement {
+                    wordCloudElements.append(safeWordCloudElement)
+                    wordCloudElement = WordCloudElement(word: word, count: 1)
+                }
+                previousWord = word
+            }
+        }
+        return wordCloudElements.sort{$1.count > $0.count}
+    }
+}
+
+extension String {
+    func stripSpecialCharacters() -> String {
+        return self.componentsSeparatedByCharactersInSet(NSCharacterSet.letterCharacterSet().invertedSet).joinWithSeparator(" ").lowercaseString
+    }
+    
+    func componentsSeparatedByWhiteSpace() -> [String] {
+        return self.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+    
+    func trimExtraWhiteSpace() -> String {
+        return self.componentsSeparatedByWhiteSpace().filter({!$0.isEmpty}).joinWithSeparator(" ")
+    }
+}
