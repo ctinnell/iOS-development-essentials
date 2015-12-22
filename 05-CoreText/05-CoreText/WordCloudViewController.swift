abe20e4ffb8c35fd0fde71b923fdae8317b2e217
Clay Tinnell <ctinnell@gaig.com>
2015-12-22 08:09:51 -0500

    Added the viewcontroller that will display the word cloud.

---
+++ 05-CoreText/05-CoreText/WordCloudViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WordCloudViewController.swift
+//  05-CoreText
+//
+//  Created by Clay Tinnell on 12/22/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class WordCloudViewController: UIViewController {
+
+    // MARK: - View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
