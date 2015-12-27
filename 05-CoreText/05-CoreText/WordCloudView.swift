198525c8100475619b2c492e2596e6bf705fdb80
Clay Tinnell <ctinnell@gaig.com>
2015-12-27 12:02:40 -0500

    Drawing direction established... Time to modularize/refactor the code.

--- 05-CoreText/05-CoreText/WordCloudView.swift
+++ 05-CoreText/05-CoreText/WordCloudView.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class WordCloudView: UIView {
+    
+    enum WordCloudDrawingDirection {
+        case Left
+        case Right
+        case Up
+        case Down
+    }
+    
+    var drawingDirection = WordCloudDrawingDirection.Right
+
 
     var wordCloudItems: [WordCloudParser.WordCloudElement]?
     
@@ -21,18 +31,93 @@ class WordCloudView: UIView {
     }
     
     func drawItems(context: CGContextRef) {
-        drawItem(context, index: 0)
-    }
-    
-    func drawItem(context: CGContextRef, index: Int) {
-        var positionX = Double(self.bounds.size.width / 2.0)
-        var positionY = Double(self.bounds.size.height / 2.0)
+        var x = Double(self.bounds.size.width / 2.0)
+        var y = Double(self.bounds.size.height / 2.0)
+        let factor = 25.0
+        
+        var wordsPerLine = 1.0
+        var wordCount = 0.0
         
         if let wordCloudItems = wordCloudItems {
-            let text = NSAttributedString(string: wordCloudItems[index].word)
-            drawText(context, text: text, x: positionX, y: positionY)
+            for item in wordCloudItems {
+                drawItem(context, item: item, x: x, y: y)
+                switch drawingDirection {
+                case .Right:
+                    if wordCount >= wordsPerLine {
+                        (x,y) = moveDown(x, y: y, factor: factor)
+                        drawingDirection = .Down
+                        wordCount = 0
+                        wordsPerLine++
+                    }
+                    else {
+                        (x,y) = moveRight(x, y: y, factor: factor)
+                        wordCount++
+                    }
+                case .Left:
+                    if wordCount >= wordsPerLine {
+                        (x,y) = moveUp(x, y: y, factor: factor)
+                        drawingDirection = .Up
+                        wordCount = 0
+                        wordsPerLine++
+                    }
+                    else {
+                        (x,y) = moveLeft(x, y: y, factor: factor)
+                        wordCount++
+                    }
+                case .Down:
+                    if wordCount >= wordsPerLine {
+                        (x,y) = moveLeft(x, y: y, factor: factor)
+                        drawingDirection = .Left
+                        wordCount = 0
+                        wordsPerLine++
+                    }
+                    else {
+                        (x,y) = moveDown(x, y: y, factor: factor)
+                        wordCount++
+                    }
+                case .Up:
+                    if wordCount >= wordsPerLine {
+                        (x,y) = moveRight(x, y: y, factor: factor)
+                        drawingDirection = .Right
+                        wordCount = 0
+                        wordsPerLine++
+                    }
+                    else {
+                        (x,y) = moveUp(x, y: y, factor: factor)
+                        wordCount++
+                    }
+                }
+        
+            }
         }
     }
+    
+    func drawItem(context: CGContextRef, item: WordCloudParser.WordCloudElement, x: Double, y: Double) {
+        let text = NSAttributedString(string: item.word)
+        drawText(context, text: text, x: x, y: y)
+    }
+    
+    func moveRight(x: Double, y: Double, factor: Double) -> (Double,Double) {
+        return (x+factor,y)
+    }
+
+    func moveDown(x: Double, y: Double, factor: Double) -> (Double,Double) {
+        return (x,y-factor)
+    }
+    
+    func moveLeft(x: Double, y: Double, factor: Double) -> (Double,Double) {
+        return (x-factor,y)
+    }
+
+    func moveUp(x: Double, y: Double, factor: Double) -> (Double,Double) {
+        return (x,y+factor)
+    }
+
+    
+//    func sizeForAttributedString(string: NSAttributedString) -> CGSize {
+//        let frameSetter = CTFramesetterCreateWithAttributedString(string)
+//        
+//    }
     
     func flipCoordinateSystem(context: CGContextRef) {
         // Flip the coordinate system
