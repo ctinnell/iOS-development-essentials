2cb592998edc0be48ef4f47c6974161c26356783
Clay Tinnell <ctinnell@gaig.com>
2016-01-07 07:38:06 -0500

    Configured Left textView

--- 06-TextKit/06-TextKit/TwoColumnViewController.swift
+++ 06-TextKit/06-TextKit/TwoColumnViewController.swift
@@ -12,10 +12,18 @@ class TwoColumnViewController: UIViewController {
 
     var text: String?
     
+    @IBOutlet weak var leftTextView: UITextView!
+    @IBOutlet weak var rightTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        initializeTextViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +31,19 @@ class TwoColumnViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initializeTextViews() {
+        initializeLeftTextView()
+        initializeRightTextView()
+    }
 
+    func initializeLeftTextView() {
+        leftTextView.text = text
+        leftTextView.scrollEnabled = false
+    }
+    
+    func initializeRightTextView() {
+        
+    }
     /*
     // MARK: - Navigation
 
