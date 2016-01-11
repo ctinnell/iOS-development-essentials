11b124b5881c5a6e60da359144ddfeb1b594e047
Clay Tinnell <ctinnell@gaig.com>
2016-01-11 17:55:37 -0500

    Added roundButtonView

---
+++ 06-TextKit/06-TextKit/RoundButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButtonView.swift
+//  06-TextKit
+//
+//  Created by Clay Tinnell on 1/11/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class RoundButtonView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
