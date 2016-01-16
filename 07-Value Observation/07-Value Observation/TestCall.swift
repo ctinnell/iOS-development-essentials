e368404a8a7598ce9ef4c57b2c571897606df04e
Clay Tinnell <ctinnell@gaig.com>
2016-01-16 09:50:03 -0500

    Reworked structs into classes and reworked execute method so that it isn't static.

---
+++ 07-Value Observation/07-Value Observation/TestCall.swift
@@ -0,0 +1,62 @@
+//
+//  TestCall.swift
+//  01-Swift-Asynchronous Network Access
+//
+//  Created by Clay Tinnell on 11/16/15.
+//  Copyright © 2015 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class TestCall {
+    var testCallResult: TestCallResult?
+    
+    enum Endpoint {
+        case Delay(Int)
+        case ImagePNG
+        case Get
+        
+        func baseURL() -> NSURL {
+            return NSURL(string: "https://httpbin.org")!
+        }
+        
+        func url() -> NSURL {
+            switch self {
+            case .Delay(let seconds):
+                return baseURL().URLByAppendingPathComponent("/delay/\(seconds)")
+            case .ImagePNG:
+                return baseURL().URLByAppendingPathComponent("/get/image/png")
+            case .Get:
+                return baseURL().URLByAppendingPathComponent("/get")
+            }
+        }
+        
+        
+    }
+    
+    func execute(url: NSURL, index:Int) {
+        let sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: sessionConfig)
+        let request = NSURLRequest(URL: url)
+        let task = session.dataTaskWithRequest(request) { [weak self] (data, response, error) -> Void in
+            if(error == nil) {
+                if let data = data {
+                    do {
+                        let jsonData = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+                        print("************************************************")
+                        print("\(jsonData)")
+                        print("************************************************")
+                    }
+                    catch let jsonError as NSError {
+                        print("Error serializing response. Error: \(jsonError)")
+                    }
+                    
+                    self?.testCallResult = TestCallResult(url: url, index: index)
+                }
+            } else {
+                print("Error: url:\(url) error\(error)")
+            }
+        }
+        task.resume()
+    }
+}
---
+++ 07-Value Observation/07-Value Observation/TestCallResult.swift
@@ -0,0 +1,19 @@
+//
+//  TestCallResult.swift
+//  07-Value Observation
+//
+//  Created by Clay Tinnell on 1/16/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class TestCallResult: NSObject {
+    var url: NSURL
+    var index: Int
+    
+    init (url: NSURL, index: Int) {
+        self.url = url
+        self.index = index
+    }
+}
