6f38acdca4b3db3bde5f3f5bba456ac2a6d698e4
Clay Tinnell <ctinnell@gaig.com>
2016-01-23 08:40:20 -0500

    Defined service protocol

---
+++ 08-WatchKit/08-WatchKit/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  08-WatchKit
+//
+//  Created by Clay Tinnell on 1/23/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    let title: String
+    let url: NSURL
+}
+
---
+++ 08-WatchKit/08-WatchKit/PhotoServiceProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoServiceProtocol.swift
+//  08-WatchKit
+//
+//  Created by Clay Tinnell on 1/23/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import Foundation
+
+protocol PhotoServiceProtocol {
+    func fetchPhotos(completion:([Photo]?, Error?) -> Void)
+}
