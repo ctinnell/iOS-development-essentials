7235caa6ce36c005194b3bd4d60c11c1c4e20ff7
Clay Tinnell <ctinnell@gaig.com>
2016-01-30 17:16:53 -0500

    Implemented Collection View and displaying images and text

---
+++ 08-WatchKit/08-WatchKit/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  08-WatchKit
+//
+//  Created by Clay Tinnell on 1/30/16.
+//  Copyright © 2016 Clay Tinnell. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var label: UILabel?
+}
