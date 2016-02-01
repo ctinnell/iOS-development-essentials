2d820b25a25988c6a2687a2500b1656a095b5137
Clay Tinnell <ctinnell@gaig.com>
2016-02-01 05:42:41 -0500

    Added photo image

--- 08-WatchKit/08-WatchKit WatchKit Extension/InterfaceController.swift
+++ 08-WatchKit/08-WatchKit WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,8 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var photoImage: WKInterfaceImage!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
